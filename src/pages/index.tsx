d2338dff5c0e83b946894c97a16b41ea6cf1d06a
FirstBright <mipo8890@gmail.com>
2024-06-01 21:18:49 +0900

    Revert "change index"
    This reverts commit ae2cd25fe3e9e67adaa16ede9ba8e7510c9d2864.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  console.log("abc")
-  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
